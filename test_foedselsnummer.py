9ca9077100afe50dff27c1bd012c63d452e83e7a
IvarKnutsen <ivar.knutsen@visma.com>
2021-11-03 16:24:07 +0100

    Another test

--- test_foedselsnummer.py
+++ test_foedselsnummer.py
@@ -5,6 +5,8 @@ from foedselsnummer import generateFNr
 class testFoedselsnummer(unittest.TestCase):
     def test_string(self):
         self.assertTrue("generateFNr(12,12,72)".isalpha)
+    def test_length(self):
+        self.assertTrue(len(generateFNr(12,12,72)) == 11)
 
 if __name__ == "__main__":
     unittest.main()
